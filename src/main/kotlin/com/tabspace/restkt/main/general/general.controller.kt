bca11565857a383c35b0c7daf08c7d4b47b1ce37
Heru Joko <herujokoutomo@gmail.com>
2020-11-20 22:44:51 +0800

    remove unecessary

--- src/main/kotlin/com/tabspace/restkt/main/general/general.controller.kt
+++ src/main/kotlin/com/tabspace/restkt/main/general/general.controller.kt
@@ -16,14 +16,3 @@ class MainCotroller {
         return body;
     }
 }
-
-class ValidationError(val violations: List<Violation>)
-class Violation(val field: String, val message: String)
-
-@ExceptionHandler(value = [MissingKotlinParameterException::class])
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-fun handleMissingKotlinParameter(exception: MissingKotlinParameterException): ValidationError {
-    val fieldName = exception.path.joinToString(separator = ".") { it.fieldName }
-    val violation = Violation(fieldName, "must not be null")
-    return ValidationError(listOf(violation))
-}
